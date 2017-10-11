ce5263390e5133d57eb55e1bde0755f915bbe486
Guest User <Guest@Epicodus-3D.local>
2017-10-11 11:11:16 -0700

    create grid

---
+++ app/src/main/java/com/epicodus/alphabet/AlphabetAdapter.java
@@ -0,0 +1,62 @@
+package com.epicodus.alphabet;
+
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class AlphabetAdapter extends BaseAdapter {
+    private Context mContext;
+    private String[] mLetters;
+    private Typeface mTypeface;
+
+    public AlphabetAdapter(Context context, String[] letters, Typeface typeface){
+        this.mContext = context;
+        this.mLetters = letters;
+        this.mTypeface = typeface;
+    }
+
+    @Override
+    public int getCount() {
+        return mLetters.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+
+        if (convertView == null) {
+            // get layout from xml file
+            gridView = inflater.inflate(R.layout.alphabet_grid_layout, null);
+
+
+            // pull views
+            TextView letterView = (TextView) gridView
+                    .findViewById(R.id.grid_item_letter);
+
+            // set values into views
+            letterView.setText(mLetters[position]);
+            letterView.setTypeface(mTypeface);
+        } else {
+            gridView = (View) convertView;
+        }
+        return gridView;
+    }
+}
---
+++ app/src/main/java/com/epicodus/alphabet/MainActivity.java
@@ -0,0 +1,24 @@
+package com.epicodus.alphabet;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+        GridView gridView;
+        String[] letters = new String[] {
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/rubik.ttf"); //change
+
+        gridView = (GridView) findViewById(R.id.baseGridView);
+
+        gridView.setAdapter(new AlphabetAdapter(this, letters, typeface));
+    }
+}
